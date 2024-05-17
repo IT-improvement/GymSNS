1883d2a7244ea048114fd267ae77521754e83aa0
it-improvement <0719improvement@naver.com>
2024-05-17 15:14:58 +0900

    Create Project

---
+++ src/main/java/util/DBManager.java
@@ -0,0 +1,52 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class DBManager {
+
+	public static Connection getConnection() {
+		Connection conn = null;
+		try {
+			Context init = new InitialContext();
+			DataSource source = (DataSource) init.lookup("java:comp/env/jdbc/GYMUnityDB");
+
+			conn = source.getConnection();
+
+			System.out.println("DB연결 성공");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println("DB연결 오류");
+		}
+
+		return conn;
+	}
+
+	public static void close(Connection conn, PreparedStatement pstmt, ResultSet rs) {
+		try {
+			conn.close();
+			pstmt.close();
+			rs.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println("DB 종료 오류");
+		}
+	}
+
+	public static void close(Connection conn, PreparedStatement pstmt) {
+		try {
+			conn.close();
+			pstmt.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println("DB 종료 오류");
+		}
+	}
+
+}
