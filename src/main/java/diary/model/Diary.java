4952dbadeb5cbab501367b330c612eb86613ca32
it-improvement <0719improvement@naver.com>
2024-05-22 11:29:01 +0900

    Add Diary : Getter,Setter Method

--- src/main/java/diary/model/Diary.java
+++ src/main/java/diary/model/Diary.java
@@ -3,5 +3,35 @@ package diary.model;
 import java.sql.Timestamp;
 
 public class Diary {
+	private int dairyIndex;
+	private int userCode;
+	private String content;
+	private Timestamp diaryDate;
+	
+	public int getDairyIndex() {
+		return dairyIndex;
+	}
+	public void setDairyIndex(int dairyIndex) {
+		this.dairyIndex = dairyIndex;
+	}
+	public int getUserCode() {
+		return userCode;
+	}
+	public void setUserCode(int userCode) {
+		this.userCode = userCode;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Timestamp getDiaryDate() {
+		return diaryDate;
+	}
+	public void setDiaryDate(Timestamp diaryDate) {
+		this.diaryDate = diaryDate;
+	}
+	
 	
 }
