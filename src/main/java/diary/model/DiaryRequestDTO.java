9cac211f6f8c401c9b67f3583358da6553675442
it-improvement <0719improvement@naver.com>
2024-05-22 11:29:48 +0900

    Add DairyRequestDto : Getter, Setter Method

--- src/main/java/diary/model/DiaryRequestDTO.java
+++ src/main/java/diary/model/DiaryRequestDTO.java
@@ -1,5 +1,28 @@
 package diary.model;
 
-public class DiaryRequestDTO {
+import java.sql.Timestamp;
 
+public class DiaryRequestDTO {
+	private int dairyIndex;
+	private String content;
+	private Timestamp diaryDate;
+	
+	public int getDairyIndex() {
+		return dairyIndex;
+	}
+	public void setDairyIndex(int dairyIndex) {
+		this.dairyIndex = dairyIndex;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Timestamp getDiaryDate() {
+		return diaryDate;
+	}
+	public void setDiaryDate(Timestamp diaryDate) {
+		this.diaryDate = diaryDate;
+	}
 }
