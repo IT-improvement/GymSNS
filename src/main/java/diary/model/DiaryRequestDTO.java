ea18e320ac7cbd0101a927521428def43af011c4
it-improvement <0719improvement@naver.com>
2024-05-22 11:59:54 +0900

    Modify DiaryRequestDTO : Delete diary_date(+getter, setter)

--- src/main/java/diary/model/DiaryRequestDTO.java
+++ src/main/java/diary/model/DiaryRequestDTO.java
@@ -1,11 +1,9 @@
 package diary.model;
 
-import java.sql.Timestamp;
 
 public class DiaryRequestDTO {
 	private int userCode;
 	private String content;
-	private Timestamp diaryDate;
 	
 	public int getUserCode() {
 		return userCode;
@@ -18,11 +16,5 @@ public class DiaryRequestDTO {
 	}
 	public void setContent(String content) {
 		this.content = content;
-	}
-	public Timestamp getDiaryDate() {
-		return diaryDate;
-	}
-	public void setDiaryDate(Timestamp diaryDate) {
-		this.diaryDate = diaryDate;
 	}
 }
