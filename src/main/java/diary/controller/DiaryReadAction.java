2f74b624cbbc7cf2a5c3360c757e0a29e44e74df
it-improvement <0719improvement@naver.com>
2024-05-24 18:51:18 +0900

    Add DiaryReadAction : CORS header

--- src/main/java/diary/controller/DiaryReadAction.java
+++ src/main/java/diary/controller/DiaryReadAction.java
@@ -1,14 +1,31 @@
 package diary.controller;
 
+import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
-public class DiaryReadAction implements Action{
+public class DiaryReadAction implements Action {
 
-	@Override
-	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException {
-		System.out.println("완료");
-	}
+    @Override
+    public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.setHeader("Access-Control-Allow-Origin", "*"); // CORS 헤더 추가
 
+        JSONObject object1 = new JSONObject();
+        object1.put("status", "200");
+        object1.put("message", "완료");
+        JSONObject object2 = new JSONObject();
+        object2.put("status", "400");
+        object2.put("message", "badrequest");
+
+        JSONArray array = new JSONArray();
+        array.put(object1);
+        array.put(object2);
+
+        response.getWriter().write(array.toString());
+    }
 }
