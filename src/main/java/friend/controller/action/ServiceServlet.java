30b5c26d4ec162d777fb03606182b3b48c23891d
typoscript <>
2024-05-27 17:10:48 +0900

    Modify Friend ServiceServlet File

--- src/main/java/friend/controller/action/ServiceServlet.java
+++ src/main/java/friend/controller/action/ServiceServlet.java
@@ -10,8 +10,11 @@ import javax.servlet.annotation.MultipartConfig;
 
 @MultipartConfig
 public class ServiceServlet extends HttpServlet {
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+		response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+		System.out.println("get friends");
 		String command = request.getParameter("command");
 		ActionFactory af = ActionFactory.getInstance();
 		
