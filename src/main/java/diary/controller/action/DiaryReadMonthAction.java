090f620f69f871aa9a622d44408fe2bc14904384
it-improvement <0719improvement@naver.com>
2024-05-28 00:01:48 +0900

    Create DiaryReadMonthAction

---
+++ src/main/java/diary/controller/action/DiaryReadMonthAction.java
@@ -0,0 +1,18 @@
+package diary.controller.action;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import diary.controller.Action;
+
+public class DiaryReadMonthAction implements Action{
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
