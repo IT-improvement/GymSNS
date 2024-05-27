bc28f6e1cc768a1cf1aeb30d17be79af2f3bc898
it-improvement <0719improvement@naver.com>
2024-05-27 16:35:43 +0900

    Add DiaryDeletAction: delete diary Logic

--- src/main/java/diary/controller/action/DiaryDeleteAction.java
+++ src/main/java/diary/controller/action/DiaryDeleteAction.java
@@ -3,14 +3,22 @@ package diary.controller.action;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import diary.controller.Action;
+import diary.model.DiaryDAO;
 
 public class DiaryDeleteAction implements Action{
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+		HttpSession session = request.getSession();
+//		String userCode = (String)session.getAttribute("user");
+		int userCode = 123;
+		int diaryIndex = Integer.parseInt(request.getParameter("diaryIndex"));
 		
+		DiaryDAO dao = DiaryDAO.getInstance();
+		dao.deleteDiary(diaryIndex);
 	}
 
 }
