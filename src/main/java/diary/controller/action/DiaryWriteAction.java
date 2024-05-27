918a94dbc770c90ddbc8a402e30283f031687294
it-improvement <0719improvement@naver.com>
2024-05-27 16:15:39 +0900

    Add DiaryWriteAction : DiaryDao & write syntax

--- src/main/java/diary/controller/action/DiaryWriteAction.java
+++ src/main/java/diary/controller/action/DiaryWriteAction.java
@@ -1,16 +1,33 @@
 package diary.controller.action;
 
+import java.sql.Timestamp;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import diary.controller.Action;
+import diary.model.DiaryDAO;
+import diary.model.DiaryRequestDTO;
 
 public class DiaryWriteAction implements Action{
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+		HttpSession session = request.getSession();
+//		String userCode = (String)session.getAttribute("user");
+		int userCode = 123;
+		String content = request.getParameter("content");
+		Timestamp diaryDate = Timestamp.valueOf(request.getParameter("diaryDate"));
 		
+		DiaryRequestDTO dto = new DiaryRequestDTO();
+		dto.setUserCode(userCode);
+		dto.setContent(content);
+		dto.setDiary_date(diaryDate);
+		
+		DiaryDAO dao = DiaryDAO.getInstance();
+		dao.writeDiary(dto);
 	}
 
 }
