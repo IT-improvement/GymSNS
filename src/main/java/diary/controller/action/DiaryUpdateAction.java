1c680f255101f3359187911b6b9e138d48f6598c
it-improvement <0719improvement@naver.com>
2024-05-27 16:21:47 +0900

    Add DiaryUpdateAction : update logic

--- src/main/java/diary/controller/action/DiaryUpdateAction.java
+++ src/main/java/diary/controller/action/DiaryUpdateAction.java
@@ -1,16 +1,33 @@
 package diary.controller.action;
 
+import java.sql.Timestamp;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import diary.controller.Action;
+import diary.model.DiaryDAO;
+import diary.model.DiaryRequestDTO;
 
 public class DiaryUpdateAction implements Action{
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+		HttpSession session = request.getSession();
+//		String userCode = (String)session.getAttribute("user");
+		int userCode = 123;
+		Timestamp date = Timestamp.valueOf(request.getParameter("diaryDate"));
+		String content = request.getParameter("content");
 		
+		DiaryRequestDTO dto = new DiaryRequestDTO();
+		dto.setUserCode(userCode);
+		dto.setContent(content);
+		dto.setDiary_date(date);
+		
+		DiaryDAO dao = DiaryDAO.getInstance();
+		dao.updateDiary(dto);
 	}
 
 }
