90d74cff4922cb91a2ea86592a07ee16bb5e871d
it-improvement <0719improvement@naver.com>
2024-05-28 11:40:50 +0900

    Create Routine: 컬럼 변수까지 작성

---
+++ src/main/java/routine/model/Routine.java
@@ -0,0 +1,35 @@
+package routine.model;
+
+public class Routine {
+	private int routineIndex;
+	private int userCode;
+	private String day;
+	private int exerciesIndex;
+	
+	public int getRoutineIndex() {
+		return routineIndex;
+	}
+	public void setRoutineIndex(int routineIndex) {
+		this.routineIndex = routineIndex;
+	}
+	public int getUserCode() {
+		return userCode;
+	}
+	public void setUserCode(int userCode) {
+		this.userCode = userCode;
+	}
+	public String getDay() {
+		return day;
+	}
+	public void setDay(String day) {
+		this.day = day;
+	}
+	public int getExerciesIndex() {
+		return exerciesIndex;
+	}
+	public void setExerciesIndex(int exerciesIndex) {
+		this.exerciesIndex = exerciesIndex;
+	}
+	
+	
+}
