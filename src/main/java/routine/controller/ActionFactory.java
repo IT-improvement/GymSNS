d3f14fddd2ba5e28f97fe40c903c1f569aa63bf9
it-improvement <0719improvement@naver.com>
2024-05-28 11:08:40 +0900

    Create Routince Action Files

---
+++ src/main/java/routine/controller/ActionFactory.java
@@ -0,0 +1,27 @@
+package routine.controller;
+
+public class ActionFactory {
+
+	private ActionFactory() {
+
+	}
+
+	private static ActionFactory instance = new ActionFactory();
+
+	public static ActionFactory getInstance() {
+		return instance;
+	}
+
+	public Action getAction(String command) {
+		Action action = null;
+
+		if (command.equals("read")) {
+		} else if (command.equals("write")) {
+		} else if (command.equals("update")) {
+		} else if (command.equals("delete")) {
+		}
+
+		return action;
+	}
+
+}
