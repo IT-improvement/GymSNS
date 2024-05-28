b7db3385b73c7efa2a21c37825e92d9d85893029
typoscript <>
2024-05-28 17:25:09 +0900

    Add File: ActionFactory

---
+++ src/main/java/exercise/controller/ActionFactory.java
@@ -0,0 +1,40 @@
+package exercise.controller;
+
+import exercise.controller.action.*;
+
+public class ActionFactory {
+	private static ActionFactory instance = new ActionFactory();
+
+	private ActionFactory() { }
+	
+	public static ActionFactory getInstance() {
+		return instance;
+	}
+	
+	public Action getAction(String command) {
+		Action action = null;
+		
+		switch (command) {
+			case "read_all":
+				action = new ExerciseReadAllAction();
+				break;
+			case "read_all_by_query":
+				action = new ExerciseReadAllByQueryAction();
+				break;
+			case "read_one":
+				action = new ExerciseReadOneAction();
+				break;
+			case "create":
+				action = new ExerciseCreateAction();
+				break;
+			case "delete":
+				action = new ExerciseDeleteAction();
+				break;
+			case "update":
+				action = new ExerciseUpdateAction();
+				break;
+		}
+
+		return action;
+	}
+}
