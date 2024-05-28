9b9320ae73363ff1ca63a7a733be31da1fe1f864
typoscript <>
2024-05-28 17:25:52 +0900

    Add File: ExerciseReadAllByQueryAction

---
+++ src/main/java/exercise/controller/action/ExerciseReadAllByQueryAction.java
@@ -0,0 +1,50 @@
+package exercise.controller.action;
+
+import exercise.controller.Action;
+import exercise.model.ExerciseDao;
+import exercise.model.ExerciseRequestDto;
+import exercise.model.ExerciseResponseDto;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+public class ExerciseReadAllByQueryAction implements Action {
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+
+		String query = request.getParameter("query");
+
+		ExerciseDao exerciseDao = ExerciseDao.getInstance();
+
+		ExerciseRequestDto exerciseDto = new ExerciseRequestDto();
+		List<ExerciseResponseDto> exercises = exerciseDao.findExerciseAllByQuery(exerciseDto, query);
+		
+		JSONArray exerciseJsonArr = new JSONArray();
+		
+		for (ExerciseResponseDto exercise : exercises) {
+			JSONObject exerciseObj = new JSONObject();
+
+			exerciseObj.put("index", exercise.getIndex());
+			exerciseObj.put("categoryIndex", exercise.getCategoryIndex());
+			exerciseObj.put("categoryName", exercise.getCategoryName());
+			exerciseObj.put("userCode", exercise.getUserCode());
+			exerciseObj.put("userId", exercise.getUserId());
+			exerciseObj.put("userName", exercise.getUserName());
+			exerciseObj.put("name", exercise.getName());
+			exerciseObj.put("content", exercise.getContent());
+			exerciseObj.put("createDate", exercise.getCreateDate());
+			exerciseObj.put("modDate", exercise.getModDate());
+			
+			exerciseJsonArr.put(exerciseObj);
+		}
+
+		response.getWriter().write(exerciseJsonArr.toString());
+	}
+}
