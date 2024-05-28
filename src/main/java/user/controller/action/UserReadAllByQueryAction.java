762fdbb286cebbc6e8e8e45f53b9ed305ad5d3bc
typoscript <>
2024-05-28 17:26:32 +0900

    Add File: UserReadAllByQueryAction

---
+++ src/main/java/user/controller/action/UserReadAllByQueryAction.java
@@ -0,0 +1,40 @@
+package user.controller.action;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import user.controller.Action;
+import user.model.UserDao;
+import user.model.UserResponseDto;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+public class UserReadAllByQueryAction implements Action {
+    @Override
+    public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        String query = request.getParameter("query");
+
+        UserDao userDao = UserDao.getInstance();
+
+        List<UserResponseDto> users = userDao.findUserAllByIdOrName(query);
+        JSONArray userJsonArr = new JSONArray();
+
+        for (UserResponseDto user : users) {
+            JSONObject userObj = new JSONObject();
+
+            userObj.put("code", user.getCode());
+            userObj.put("id", user.getId());
+            userObj.put("name", user.getName());
+
+            userJsonArr.put(userObj);
+        }
+
+        response.getWriter().write(userJsonArr.toString());
+    }
+}
