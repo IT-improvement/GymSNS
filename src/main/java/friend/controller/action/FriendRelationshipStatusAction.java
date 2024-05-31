a67e894515347da5a854fcc0951897dc81daa4c5
typoscript <>
2024-05-31 17:43:27 +0900

    Add File: FriendRelationshipStatusAction

---
+++ src/main/java/friend/controller/action/FriendRelationshipStatusAction.java
@@ -0,0 +1,54 @@
+package friend.controller.action;
+
+import friend.controller.Action;
+import friend.model.FriendDao;
+import friend.model.FriendRequestDto;
+import friendRequest.model.FriendRequestDao;
+import friendRequest.model.FriendRequestRequestDto;
+import org.json.JSONObject;
+import util.ApiResponseManager;
+import util.ParameterValidator;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class FriendRelationshipStatusAction implements Action {
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+
+		String userCodeStr = request.getHeader("Authorization");
+		String userCodeOtherStr = request.getParameter("userCodeOther");
+
+		JSONObject resObj = new JSONObject();
+
+		if (!ParameterValidator.isInteger(userCodeStr) || !ParameterValidator.isInteger(userCodeOtherStr)) {
+			response.sendError(400, "Bad Request");
+			return;
+		}
+
+		int userCode = Integer.parseInt(userCodeStr);
+		int userCodeOther = Integer.parseInt(userCodeOtherStr);
+
+		FriendRequestDao friendRequestDao = FriendRequestDao.getInstance();
+		FriendRequestRequestDto friendRequestDto = new FriendRequestRequestDto(userCode, userCodeOther);
+
+		FriendDao friendDao = FriendDao.getInstance();
+		FriendRequestDto friendDto = new FriendRequestDto(userCode, userCodeOther);
+
+		if (friendRequestDao.isUserInSentFriendRequest(friendRequestDto)) {
+			resObj.put("relationshipStatus", "friendRequestSent");
+		} else if (friendRequestDao.isUserInReceivedFriendRequest(friendRequestDto)) {
+			resObj.put("relationshipStatus", "friendRequestReceived");
+		} else if (friendDao.isFriend(friendDto)) {
+			resObj.put("relationshipStatus", "friend");
+		} else {
+			resObj.put("relationshipStatus", "notFriend");
+		}
+
+		response.getWriter().write(resObj.toString());
+	}
+}
