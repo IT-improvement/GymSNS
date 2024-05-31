a2f95807cca9d334d495fa324ad38d703bcba378
it-improvement <0719improvement@naver.com>
2024-05-31 10:23:46 +0900

    Add RoutineRequestDTO: add date(type: TimeStamp)

--- src/main/java/routine/model/RoutineRequestDTO.java
+++ src/main/java/routine/model/RoutineRequestDTO.java
@@ -1,10 +1,20 @@
 package routine.model;
 
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+
 public class RoutineRequestDTO {
 	private String day;
 	private int routineIndex;
 	private int exerciseIndex;
 	private int userCode;
+	private Timestamp date;
+	
+	public RoutineRequestDTO() {
+		SimpleDateFormat sdf= new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+		String time = sdf.format(System.currentTimeMillis());
+		date = Timestamp.valueOf(time);
+	}
 	
 	public String getDay() {
 		return day;
