4e31b58e3ad630c40b151b33b7e7855193acafc9
codekingjj <jaejung0413@naver.com>
2024-05-31 17:38:52 +0900

    Edit : FeedDeleteAction,FeedDetailAction

--- src/main/java/feed/model/FeedDAO.java
+++ src/main/java/feed/model/FeedDAO.java
@@ -269,7 +269,8 @@ public class FeedDAO {
 		return feed;
 	}
 
-	public void deleteFeed(FeedRequestDTO feedDto) {
+	public boolean deleteFeed(FeedRequestDTO feedDto) {
+		boolean isDeleted = true;
 		try {
 			conn = DBManager.getConnection();
 			System.out.println(feedDto.getFeedIndex());
@@ -281,9 +282,11 @@ public class FeedDAO {
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
+			isDeleted = false;
 		}finally {
 			DBManager.close(conn, pstmt);
 		}
+		return isDeleted;
 	}
 
 	public ArrayList<Feed> readFeedFavoriteInfoAll(ArrayList<Feed> list) {
