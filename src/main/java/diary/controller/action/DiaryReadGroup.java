707358e6ba1e6ed1afc18c227c4e713368de118f
jujae <jujae@seungjae>
2024-06-05 12:13:36 +0900

    Add DiaryReadGroup: readGroup files

---
+++ src/main/java/diary/controller/action/DiaryReadGroup.java
@@ -0,0 +1,45 @@
+package diary.controller.action;
+
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import diary.controller.Action;
+import diary.model.Diary;
+import diary.model.DiaryDAO;
+import diary.model.DiaryResponseDTO;
+
+public class DiaryReadGroup implements Action{
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.setHeader("Access-Control-Allow-Origin", "*"); // CORS 헤더 추가
+        
+        int userCode = 123;
+        int number = Integer.parseInt(request.getParameter("number"));
+        DiaryDAO dao = DiaryDAO.getInstance();
+        List<DiaryResponseDTO> diaryListItem = dao.readDiaryGroup5(userCode, number);
+        JSONArray array = new JSONArray();
+        
+        for(DiaryResponseDTO diary : diaryListItem) {
+        	JSONObject object = new JSONObject();
+        	object.put("diary_index", diary.getDairyIndex());
+        	object.put("user_cod", diary.getUserCode());
+        	object.put("content", diary.getContent());
+        	object.put("diary_date", diary.getDiaryDate());
+        	array.put(object);
+        }
+        
+        response.getWriter().write(array.toString());
+	}
+
+}
